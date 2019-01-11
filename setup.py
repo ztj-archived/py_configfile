b7f34a273c1a332d148cc66c5c2c9d1e2dd41a21
ZhangTianJie <715751607@qq.com>
2019-01-11 17:03:04 +0800

    优化了 setup.py
    -  修正了 setup.py 文档不展示问题

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
+# Author: ZhangTianJie
 
-setup(
+import setuptools
+
+with open('README.md', 'r', encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='py_configfile',
-    version='0.0.1',
+    version='0.0.2',
     description='python configuration file loading package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     py_modules=["configfile"],
 
     url='http://github.com/ztj1993/py_configfile',
