1e0bd8b93ffe90f51bc084740e09f5b8bb90936c
ZhangTianJie <715751607@qq.com>
2019-01-14 16:39:53 +0800

    修正了 long_description 问题

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 
 import setuptools
 
-with open('README.md', 'r', encoding='utf8') as fh:
-    long_description = fh.read()
+try:
+    long_description = open('README.md', encoding='utf8').read()
+except:
+    long_description = 'python configuration file loading package',
 
 setuptools.setup(
     name='py_configfile',
-    version='0.0.2',
+    version='0.0.3',
     description='python configuration file loading package',
     long_description=long_description,
     long_description_content_type="text/markdown",
